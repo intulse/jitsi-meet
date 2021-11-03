3802f7485f9dd12ea0487c9195559fd635be95c6
Brent Keath <brent.keath@intulse.com>
2021-11-03 13:49:39 -0400

    Enabling simulcast
    Disabling it may be causing video issues

--- config.js
+++ config.js
@@ -198,7 +198,7 @@ var config = {
     },
 
     // Enable / disable simulcast support.
-    disableSimulcast: true,
+    disableSimulcast: false,
 
     // Enable / disable layer suspension.  If enabled, endpoints whose HD layers are not in use will be suspended
     // (no longer sent) until they are requested again. This is enabled by default. This must be enabled for screen
